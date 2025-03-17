94ffa1d4a80e8e04ce3fa5caf8e87e412f70f718
Jose Borges <klarck345@gmail.com>
2025-03-17 15:51:48 -0400

    added seeds and modified schema

--- Data/schema.sql
+++ Data/schema.sql
@@ -2,28 +2,32 @@ DROP DATABASE IF EXISTS Employee_db;
 
 CREATE DATABASE Employee_db;
 -- new database created
+USE Employee_db;
 
 -- department table: id, name
-CREATE TABLE Employee_db.Department (
+CREATE TABLE Department (
     id SERIAL PRIMARY KEY,
-    name VARCHAR(30) UNIQUE NOT NULL,
+    name VARCHAR(30) UNIQUE NOT NULL
 );
 
 -- role table: id, title, salary, department_id
-CREATE TABLE Employee_db.Role (
+CREATE TABLE Role (
     id SERIAL PRIMARY KEY,
     title VARCHAR(30) UNIQUE NOT NULL,
     salary DECIMAL(10, 2) NOT NULL,
     department_id INTEGER NOT NULL,
+    FOREIGN KEY (department_id) REFERENCES Department (id)
 );
 
 
 
 -- employee table: id, first_name, last_name, role_id, manager_id
-CREATE TABLE Employee_db.Employee (
+CREATE TABLE Employee (
     id SERIAL PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
     role_id INTEGER NOT NULL,
-    manager_id  INTEGER NOT NULL,
+    manager_id  INTEGER,
+    FOREIGN KEY (role_id) REFERENCES Role (id), 
+    FOREIGN KEY (manager_id) REFERENCES Employee (id)
 );
--- Data/seeds.sql
+++ Data/seeds.sql
@@ -2,8 +2,29 @@
 
 -- INSERT INTO Employee_db.Department (name) VALUES
 
-INSERT INTO Employee_db.Department (name) VALUES
-('Engineering', 'Veronica Pierce', 'Monica Spencer'),
-('Finance', 'Keith Johnson', 'Lenore Williams'),
-('Legal', 'Kamala Khan', 'Louise Lane'),
-('Sales', 'Diana Prince', 'Bruce Wayne');
+INSERT INTO Employee_db.Department (name) 
+VALUES ('Engineering', 'Veronica Pierce', 'Monica Spencer'),
+       ('Finance', 'Keith Johnson', 'Lenore Williams'),
+       ('Legal', 'Kamala Khan', 'Louise Lane'),
+       ('Sales', 'Diana Prince', 'Bruce Wayne');
+
+-- INSERT INTO Employee_db.Role (title, salary, department_id) VALUES
+
+INSERT INTO Employee_db.Role (title, salary, department_id) 
+VALUES ('Engineer', 100000, 1),
+       ('Manager', 150000, 1),
+       ('Accountant', 125000, 2),
+       ('Lawyer', 120000, 3),
+       ('Salesperson', 100000, 4);
+
+-- INSERT INTO Employee_db.Employee (first_name, last_name, role_id, manager_id) VALUES
+
+INSERT INTO Employee_db.Employee (first_name, last_name, role_id, manager_id) 
+VALUES ('Veronica', 'Pierce', 1, 2),
+       ('Monica', 'Spencer', 2, 2),
+       ('Keith', 'Johnson', 3, 4),
+       ('Lenore', 'Williams', 4, 4),
+       ('Kamala', 'Khan', 5, 6),
+       ('Louise', 'Lane', 6, 6),
+       ('Diana', 'Prince', 7, 8),
+       ('Bruce', 'Wayne', 8, 8);
