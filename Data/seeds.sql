817efb1850d5c9575d5591092dd8badbcde45d26
Jose Borges <klarck345@gmail.com>
2025-03-16 23:42:25 -0400

    started list on seeds.sql file

---
+++ Data/seeds.sql
@@ -0,0 +1,9 @@
+-- Employee list: first_name, last_name, role_id, manager_id
+
+-- INSERT INTO Employee_db.Department (name) VALUES
+
+INSERT INTO Employee_db.Department (name) VALUES
+('Engineering', 'Veronica Pierce', 'Monica Spencer'),
+('Finance', 'Keith Johnson', 'Lenore Williams'),
+('Legal', 'Kamala Khan', 'Louise Lane'),
+('Sales', 'Diana Prince', 'Bruce Wayne');
